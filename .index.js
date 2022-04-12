859dc5f24400adcb83e959b73ab51d945cbcf13a
sergiojara31 <sergio.jaramillob@autonoma.edu.co>
2022-04-12 16:58:13 -0500

    mandar mensajes con twilio

--- .index.js
+++ .index.js
@@ -31,6 +31,16 @@ app.use(express.json());
 routerApi(app);
 
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+19893129428',
+     to: '+573116354327'
+   })
+  .then(message => console.log(message.sid));
 
 
